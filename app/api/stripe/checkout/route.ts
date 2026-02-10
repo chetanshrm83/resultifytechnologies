14cabca2c0076409adeeafdecd10dfea0eca5247
chetanshrm83 <chetanshrm83@gmail.com>
2026-02-11 02:24:29 +0530

    Update route.ts

--- app/api/stripe/checkout/route.ts
+++ app/api/stripe/checkout/route.ts
@@ -2,8 +2,9 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-04-10",
+  apiVersion: "2023-10-16",
 });
+
 
 export async function POST(req: Request) {
   try {
