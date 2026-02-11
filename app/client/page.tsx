797d5185eb204258836dcc4b3d166902c414f290
chetanshrm83 <chetanshrm83@gmail.com>
2026-02-11 08:55:39 +0530

    Update page.tsx

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { getSupabase } from "@/lib/supabaseClient";
+import { supabase } from "../../lib/supabaseClient";
 const supabase = getSupabase();
 export default function ClientLogin() {
   const supabase = getSupabase();
